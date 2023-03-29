91e958ee4a1fdb00044e4c595cd07634a3033ccb
Noel <noel.gebhardt@gmail.com>
2023-03-29 16:49:04 +0200

    added text & css

--- src/App.js
+++ src/App.js
@@ -2,58 +2,122 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import mars from './mars.png'
 import chicken from './img-withoutbg.png'
+import rocket from './rocket-ship.png'
 import {Parallax, ParallaxLayer} from "@react-spring/parallax";
-import {Container} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import "aos/dist/aos.css";
 import Zoom from 'react-reveal/Zoom.js';
 import Roll from 'react-reveal/Roll.js';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown.d.ts';
 
 function App() {
-/*    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-    }, []);*/
+    /*    useEffect(() => {
+            AOS.init();
+            AOS.refresh();
+        }, []);*/
 
-    return (
-        <Parallax pages={4}>
-            <Container>
-                <ParallaxLayer
-                    offset={1}
-                    speed={0.1}
-                    factor={2}
-                    style={{
-                        backgroundImage: `url(${mars})`,
-                        backgroundSize: '80%'
-                    }
-                    }/>
-                <ParallaxLayer
-                    offset={1.8}
-                    speed={1.5}
-                    factor={2}
-                    style={{
-                        backgroundImage: `url(${chicken})`,
-                        backgroundSize: '20%'
-                    }
-                    }/>
-                <ParallaxLayer offset={0.2} speed={0}>
-                    <h1 className="text-center">Welcome to chicken planet!</h1>
-                    <br/><br/><br/>
-                    <h2 className="text-center">Scroll down to continue...</h2>
-                    <br/><br/>
-                    <KeyboardDoubleArrowDownIcon/>
-                </ParallaxLayer>
-                <div className="mydiv"/>
+    return (<Parallax pages={5}>
+        <Container>
+            <ParallaxLayer
+                offset={1}
+                speed={0.1}
+                factor={2}
+                style={{
+                    backgroundImage: `url(${mars})`, backgroundSize: '80%', backgroundPosition: 'center'
+                }}/>
+            <ParallaxLayer
+                offset={1.8}
+                speed={1.5}
+                factor={2}
+                style={{
+                    backgroundImage: `url(${chicken})`, backgroundSize: '20%'
+                }}/>
+
+
+            <ParallaxLayer offset={0.2} speed={0}>
+                <h1 className="text-center">Welcome to chicken planet!</h1>
+                <br/><br/><br/>
+                <h2 className="text-center">Scroll down to continue...</h2>
+                <br/><br/>
+                <KeyboardDoubleArrowDownIcon/>
+            </ParallaxLayer>
+            <div className="mydiv">
                 <Zoom>
-                    <p>Markup that will be revealed on scroll</p>
                 </Zoom>
-                <Roll right>
-                    <h1>hhhddddddddd</h1>
+                <Roll left>
+                    <h1>Explore Chickentopia</h1>
+                    <h5>A Poultry Lover's Dream Destination!</h5>
+                    <p>Embark on a journey to Chickentopia, a planet unlike any other, where chickens reign supreme.
+                        This 3-day adventure will take you on a tour of the planet's vibrant markets, chicken farms,
+                        and
+                        scenic landscapes.</p>
+                    <p>
+                        You'll have the chance to participate in traditional chicken-themed activities, such as egg
+                        collecting and chicken racing. Plus, you'll enjoy mouthwatering chicken dishes, prepared
+                        with
+                        locally sourced ingredients by skilled chefs.</p>
+                    <br/>
+                    <h4>1'500$</h4>
                 </Roll>
-                {/*https://www.react-reveal.com/*/}
-                {/* https://stablediffusionweb.com*/}
+            </div>
+            <br/><br/><br/>
+            <Row>
+                <Col className="col-6"/>
+                <Col className="col-6 divright">
+                    <Roll right>
+                        <h1>Welcome to Clucktopolis</h1>
+                        <h5>A Planet of Feathers and Fun</h5>
+                        <p>Step into Chickentopia, a planet that's home to all things chicken. This 5-day excursion will
+                            take you on a tour of the planet's lively chicken markets, sprawling chicken farms, and
+                            stunning landscapes.</p>
+                        <p>
+                            Experience the joy of watching chicks hatch and roam free on the farms. Discover the
+                            fascinating history and culture of chickens and their significance in the planet's
+                            society.</p>
+                        <br/>
+                        <h4>2'500$</h4>
+                    </Roll>
+                </Col>
+            </Row>
+            <br/><br/><br/>
+            <Row>
+                <Col className="col-6">
+                    <Roll right>
+                        <h1>Chirp World</h1>
+                        <h5>A Journey to the Heart of Chicken Paradise</h5>
+                        <p>Welcome to Chirp World, a planet entirely dedicated to our feathered friends. This 7-day
+                            adventure will take you on a journey to explore the planet's vibrant markets, chicken
+                            sanctuaries, and natural habitats.</p>
+                        <p>
+                            Savor the mouth-watering chicken dishes, prepared by expert chefs using locally sourced
+                            ingredients. The planet is also home to various chicken-inspired art and cultural
+                            activities, so there is something for everyone.</p>
+                        <br/>
+                        <h4>3'500$</h4>
+                    </Roll>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-6"/>
+                <Col className="col-6 divright">
+                    <Roll right>
+                        <h1>Featherlandia</h1>
+                        <h5>An Avian Adventure Like No Other</h5>
+                        <p>Welcome to Featherlandia, a planet where the sky is always filled with the graceful flapping of wings. This 4-day adventure will take you on a tour of the planet's unique chicken markets, expansive aviaries, and stunning natural wonders.</p>
+                        <p>
+                            Explore the rich history and culture of chickens, and their significance to Featherlandia's traditions and customs. The planet's vibrant art scene is also dedicated to the beauty of birds, with intricate feather art and sculptures decorating the streets.</p>
+                        <br/>
+                        <h4>1'800$</h4>
+                    </Roll>
+                </Col>
+            </Row>
+            <br/><br/><br/>
+            <Row>
 
-                {/*            <div className="vh-100 d-flex bg-primary"></div>
+            {/*https://www.react-reveal.com/*/}
+            {/* https://stablediffusionweb.com*/}
+
+            {/*            <div className="vh-100 d-flex bg-primary"></div>
                 <div data-aos="fade-down-right">
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
                     ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
@@ -64,9 +128,8 @@ function App() {
                     ipsum dolor sit amet.
                 </div>
                 <div className="vh-100 d-flex bg-primary"></div>*/}
-            </Container>
-        </Parallax>
-    );
+        </Container>
+    </Parallax>);
 }
 
 export default App;
